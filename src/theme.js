12b6d35a62943eb1617498c2d0540c63c32f26f6
cs572go <tushartushar572@gmail.com>
2023-05-01 12:51:21 +0530

    done

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,22 @@
 import { createContext } from "react";
 import { createTheme } from "@mui/material/styles";
 
+import React from 'react'
+
+export const tokens = (mode) => ({
+  ...(mode==='dark'
+    ?{
+        // paste hex codes
+    }
+  )
+})
+
+export const themeSettings = () => {
+  const colors = tokens(mode);
+
+  return{
+    palette: {
+        
+    }
+  }
+}
